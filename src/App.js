741fffc090354be52f30ea1aaed29ac0572ac2a1
axel <axel-bentancur@hotmail.com>
2020-08-31 20:15:30 -0300

    winery home page design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="main-nav">
+        <div className="main-nav-logo">
+          <h1>PSITIO</h1>
+        </div>
+        <ul className="main-nav-list">
+          <li className="main-nav-item">
+            <a href="#Home" className="main-nav-link">
+              Home
+            </a>
+          </li>
+          <li className="main-nav-item">
+            <a href="#Home" className="main-nav-link">
+              About
+            </a>
+          </li>
+          <li className="main-nav-item">
+            <a href="#Home" className="main-nav-link">
+              Wines
+            </a>
+          </li>
+          <li className="main-nav-item">
+            <a href="#Home" className="main-nav-link">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="content">
+        <div className="side-left-content">
+          <div className="side-left">
+            <div className="links">
+              <ul className="links-list">
+                <li className="link-item">
+                  <a href="#instagram" className="link-link">
+                    Instagram
+                  </a>
+                </li>
+                <li className="link-item">
+                  <a href="#twitter" className="link-link">
+                    Twitter
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="side-middle">
+          <div className="middle-content">
+            <h2>Lorem ipsum dolor sit amet consectetur adipisicing.</h2>
+            <div className="line">
+              <span className="line-num">Harvest</span>
+              <span className="middle-line">a </span>
+              <span className="line-num">87</span>
+            </div>
+          </div>
+        </div>
+        <div className="side-right-content">
+          <div className="side-right">
+            <div className="links">
+              <ul className="links-list">
+                <li className="link-item">
+                  <a href="#email" className="link-link">
+                    psitio@email.com
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
